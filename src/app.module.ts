b2e92f482fcaba02519792d3db1fa3ade038263b
yu-ishii-yumemi <yu_ishii@yumemi.co.jp>
2023-06-08 10:19:37 +0900

    feat: validator middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { PetModule } from './api/pet/pet.module';
+import { join } from 'path';
+import * as OpenApiValidator from 'express-openapi-validator';
 
 @Module({
   imports: [PetModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        ...OpenApiValidator.middleware({
+          apiSpec: join(__dirname, './petstore.yaml'),
+          validateFormats: 'full',
+        }),
+      )
+      .forRoutes('*');
+  }
+}
